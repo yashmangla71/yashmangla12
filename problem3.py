ede3e4f1c772d2efd7801911246e2607f59745af
yashmangla71 <61405824+yashmangla71@users.noreply.github.com>
2020-02-24 17:32:34 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,3 +1,5 @@
-Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+num = int(input())
+bin_n = bin(num)[2:]
+one_l = [i for i in bin_n.split("0") if i != '']
+one_l.sort(key=len)
+print(len(one_l[-1]))
